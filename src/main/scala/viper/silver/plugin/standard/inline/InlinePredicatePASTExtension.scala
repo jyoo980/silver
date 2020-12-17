dab442748fb13b69d7e6b57bd8f59aadb6c8c46a
James Yoo <jyoo980@gmail.com>
2020-12-16 20:01:11 -0800

    adding scratch work for translateMember

--- src/main/scala/viper/silver/plugin/standard/inline/InlinePredicatePASTExtension.scala
+++ src/main/scala/viper/silver/plugin/standard/inline/InlinePredicatePASTExtension.scala
@@ -1,7 +1,7 @@
 package viper.silver.plugin.standard.inline
 
 import viper.silver.parser._
-import viper.silver.ast.Member
+import viper.silver.ast.{Member, Predicate}
 
 sealed trait PInlinePredicateDeclaration extends PExtender with PMember with PGlobalDeclaration {}
 
@@ -9,5 +9,13 @@ case class PInlinePredicate(predicate: PPredicate) extends PInlinePredicateDecla
   override def idndef: PIdnDef = predicate.idndef
   override def getSubnodes(): Seq[PNode] = Seq(predicate)
   override def typecheck(t: TypeChecker, n: NameAnalyser): Option[Seq[String]] = None // TODO
-  override def translateMember(t: Translator): Member = InlinePredicate(???)(t.liftPos(predicate)) // TODO
+
+  override def translateMember(t: Translator): Member = {
+    val name = predicate.idndef.name
+    val formalArgs = predicate.formalArgs.map(t.liftVarDecl)
+    val maybeBody = predicate.body.map(t.exp)
+    InlinePredicate(
+      Predicate(name, formalArgs, maybeBody)() // TODO: not too sure about the curried argument list
+    )(t.liftPos(predicate))
+  }
 }
